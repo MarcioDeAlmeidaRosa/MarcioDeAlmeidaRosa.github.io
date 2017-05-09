fcaf4c66a5b9378d7e87740ee4a955fa91922ce8
DESKTOP-Q676H36\Marcio Rosa <marcio.almeida.rosa@gmail.com>
2017-05-09 17:53:51 -0300

    Aplicando recursos responsivos de layout

---
+++ js/responsivo.js
@@ -0,0 +1,18 @@
+document.querySelector('.menu-abrir').onclick = function() {
+    document.documentElement.classList.add('menu-ativo');
+};
+
+document.querySelector('.menu-fechar').onclick = function() {
+    document.documentElement.classList.remove('menu-ativo');
+};
+
+document.documentElement.onclick = function(event) {
+    if (event.target === document.documentElement) {
+        document.documentElement.classList.remove('menu-ativo');
+    }
+};
+
+document.querySelector('.itens-menu').onclick = function() {
+    // TODO - TEMPORÁRIO
+    document.documentElement.classList.remove('menu-ativo');
+};
